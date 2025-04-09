8f5d41081acc4d05645e793909bc62eb06a37dbb
Stijn De Pestel <stijn.depestel@gmail.com>
2025-04-09 14:35:22 +0200

    Switch to FZF and reduce font size.

--- nvim/lua/plugins/worktree.lua
+++ nvim/lua/plugins/worktree.lua
@@ -19,7 +19,7 @@ end
 return {
   "ThePrimeagen/git-worktree.nvim",
   config = function()
-    require("telescope").load_extension("git_worktree")
+    -- require("telescope").load_extension("git_worktree")
     vim.keymap.set("n", "<leader>gwl", "<CMD>lua require('telescope').extensions.git_worktree.git_worktrees()<CR>")
     vim.keymap.set("n", "<leader>gwa", createNewWorkTree)
   end,
