fe8eb16293ca35d9a65bf86ca1bc224a0bdb3c34
Stijn De Pestel <stijn.depestel@gmail.com>
2025-04-24 14:37:05 +0200

    QoL & updates

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -7,3 +7,6 @@ vim.keymap.set("n", "<C-h>", ":TmuxNavigateLeft<CR>", { desc = "Navigate Left" }
 vim.keymap.set("n", "<C-l>", ":TmuxNavigateRight<CR>", { desc = "Navigate Right" })
 vim.keymap.set("n", "<C-j>", ":TmuxNavigateDown<CR>", { desc = "Navigate Down" })
 vim.keymap.set("n", "<C-k>", ":TmuxNavigateUp<CR>", { desc = "Navigate Up" })
+
+-- Do not move deleted contents with "d" to the default register aka the clipboard.
+vim.keymap.set({ "n", "v" }, "d", '"_d', { noremap = true, silent = true })
