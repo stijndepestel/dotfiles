1319e2cc688ecc4de7507d8d48f4ac978ef01b3b
Stijn De Pestel <stijn.depestel@aikido.dev>
2025-12-19 17:23:05 +0100

    Remove nvim autocmds and update ide terminal layout shortcut.

--- tmux/layouts/ide.sh
+++ tmux/layouts/ide.sh
@@ -4,11 +4,12 @@
 unique_tops=$(tmux list-panes -F "#{pane_top}" | sort -u | wc -l)
 if [ "$unique_tops" -lt 2 ]; then
   tmux split-window -v -p 20 -c "#{pane_current_path}"
-else 
+else
   # Make the top pane take up 80% of the window height.
   total=$(tmux display-message -p "#{window_height}")
   top=$(((total * 75) / 100))
   top_pane=$(tmux list-panes -F "#{pane_id} #{pane_top}" | sort -nk2 | head -n1 | awk '{print $1}')
   tmux resize-pane -t "$top_pane" -y "$top"
+  bottom_left_pane=$(tmux list-panes -F "#{pane_id} #{pane_top} #{pane_left}" | sort -k2,2nr -k3,3n | head -n1 | awk '{print $1}')
+  tmux select-pane -t "$bottom_left_pane"
 fi
-
